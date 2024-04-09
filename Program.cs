3e6064b865532b74a969830301456f89f9e4a3e7
João Bosco <ti.junior@gmail.com>
2024-04-09 14:21:14 -0300

    Configuração do Entity Framework e Conexão com o Banco.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebCadastro.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
